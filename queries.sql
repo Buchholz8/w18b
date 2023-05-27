99250235872650f44faa0ec61a47d47c9d3a27a0
markb <markbuchholz8@gmail.com>
2023-05-27 08:59:19 -0600

    fixes to queries.sql

--- queries.sql
+++ queries.sql
@@ -1,141 +1,43 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
---
--- Host: localhost    Database: w18b
--- ------------------------------------------------------
--- Server version	5.5.5-10.6.13-MariaDB
+insert into customer(username, password, created_at) values ('user one' , 'password' , '2012-05-13');
+insert into customer(username, password, created_at) values ('user two' , 'pass' , '2023-04-03');
+insert into customer(username, password, created_at) values ('user three' , 'doesthiswork' , '2009-02-12');
+insert into customer(username, password, created_at) values ('user four' , 'somethinghard' , '2003-01-01');
+insert into customer(username, password, created_at) values ('user five' , 'tuffwords' , '1999-11-08');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+insert into item(name, price, description, costomer_id) values('thing one' , 1 , 'this is a great thing one');
+insert into item(name, price, description) values('thing two' , 2 , 'this is a great thing two');
+insert into item(name, price, description) values('thing three' , 3 , 'this is a great thing three');
+insert into item(name, price, description) values('thing four' , 4 , 'this is a great thing four');
+insert into item(name, price, description) values('thing five' , 5 , 'this is a great thing five');
+insert into item(name, price, description) values('thing six' , 6 , 'this is a great thing six');
+insert into item(name, price, description) values('thing seven' , 7 , 'this is a great thing seven');
+insert into item(name, price, description) values('thing nine' , 9 , 'this is a great thing nine');
+insert into item(name, price, description) values('thing ten' , 10 , 'this is a great thing ten');
 
---
--- Table structure for table `customer`
---
+insert into `order`(created_at, customer_id) values('2023-01-01 12:00:00' , '1');
+insert into `order`(created_at, customer_id) values('2023-05-02 09:00:00' , '1');
+insert into `order`(created_at, customer_id) values('2023-05-03 11:45:00' , '3');
+insert into `order`(created_at, customer_id) values('2023-05-04 10:00:00' , '4');
+insert into `order`(created_at, customer_id) values('2023-05-05 02:15:56' , '5');
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer` (
-  `username` varchar(30) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `costomer_un` (`username`),
-  UNIQUE KEY `customer_un` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+insert into order_item(order_id, item_id) values('1' ,'1');
+insert into order_item(order_id, item_id) values('2' ,'2');
+insert into order_item(order_id, item_id) values('3' ,'3');
+insert into order_item(order_id, item_id) values('4' ,'4');
+insert into order_item(order_id, item_id) values('5' ,'5');
+insert into order_item(order_id, item_id) values('5' ,'6');
+insert into order_item(order_id, item_id) values('5' ,'7');
+insert into order_item(order_id, item_id) values('5' ,'8');
+insert into order_item(order_id, item_id) values('4' ,'9');
+insert into order_item(order_id, item_id) values('4' ,'10');
+insert into order_item(order_id, item_id) values('3' ,'10');
+insert into order_item(order_id, item_id) values('3' ,'9');
+insert into order_item(order_id, item_id) values('2' ,'8');
+insert into order_item(order_id, item_id) values('2' ,'7');
+insert into order_item(order_id, item_id) values('1' ,'6');
 
---
--- Dumping data for table `customer`
---
+select customer.username from customer inner join `order` on customer.id = `order`.customer_id;
+select customer.username , `order`.customer_id from customer inner join `order` on customer.id = `order`.customer_id;mkdir
+select avg(item.price) , order_item.item_id from item inner join order_item on item.id = order_item.item_id;
+select customer.username , max(item.price) as highestprice from customer inner join `order` on customer.id = `order`.customer_id inner join `order` on order_item.order_id = `order`.id inner join order_item on item.id = order_item.item_id ;
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES ('user one','password','2020-04-01',1),('user two','pass','2023-04-03',3),('user three','doesthiswork','2009-02-12',4),('user four','somethinghard','2003-01-01',5),('user five','tuffwords','1999-11-08',6);
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `item`
---
-
-DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `item` (
-  `name` varchar(30) DEFAULT NULL,
-  `price` decimal(10,0) DEFAULT NULL,
-  `description` varchar(250) DEFAULT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item`
---
-
-LOCK TABLES `item` WRITE;
-/*!40000 ALTER TABLE `item` DISABLE KEYS */;
-INSERT INTO `item` VALUES ('thing one',1,'this is a great thing one',1),('thing two',2,'this is a great thing two',2),('thing three',3,'this is a great thing three',3),('thing four',4,'this is a great thing four',4),('thing five',5,'this is a great thing five',5),('thing six',6,'this is a great thing six',6),('thing seven',7,'this is a great thing seven',7),('thing eight',8,'this is a great thing eight',8),('thing nine',9,'this is a great thing nine',9),('thing ten',10,'this is a great thing ten',10);
-/*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order` (
-  `created_at` datetime DEFAULT NULL,
-  `customer_id` int(10) unsigned DEFAULT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  KEY `order_FK` (`customer_id`),
-  CONSTRAINT `order_FK` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES ('2023-01-01 12:00:00',1,1),('2023-05-02 09:00:00',1,2),('2023-05-03 11:45:00',3,3),('2023-05-04 10:00:00',4,4),('2023-05-05 02:15:56',5,5);
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order_item`
---
-
-DROP TABLE IF EXISTS `order_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_item` (
-  `order_id` int(10) unsigned DEFAULT NULL,
-  `item_id` int(10) unsigned DEFAULT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  KEY `order_item_FK` (`order_id`),
-  KEY `order_item_FK_1` (`item_id`),
-  CONSTRAINT `order_item_FK` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `order_item_FK_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_item`
---
-
-LOCK TABLES `order_item` WRITE;
-/*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
-INSERT INTO `order_item` VALUES (2,2,1),(3,3,2),(4,4,3),(5,5,4),(5,6,5),(5,7,6),(5,8,7),(4,9,8),(4,10,9),(3,10,10),(3,9,11),(2,8,12),(2,7,13),(1,6,14),(1,6,15);
-/*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'w18b'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-05-22 20:36:08
